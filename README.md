e8f8d490339d8a1d108211d6fcc30259ce179b79
chiotas <99977091+chiotas@users.noreply.github.com>
2026-02-16 15:43:52 +0100

    0.9.1
    0.9.1

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ I am not a meteorologist (and probably not even a photographer), so all construc
 If you need further assistance or another translation, feel free to ask!
 
 # CHANGELOG
--   0.9
+-   0.9.1
     - Mobile version compatible!!
     - Adaptive rings: now if you select a specific clouds layer, the plugin will show you the rings only for that clouds layer 
     - Code tuned for the new Leaflet-gl Windy.com plugin ecosystem 
--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-horizon-distance',
-    version: '0.9.0',  
+    version: '0.9.1',  
     title: 'Clouds Horizon Distance',
     description: 'This plugin displays circles on the Windy map representing the horizon distances for different cloud heights, calculated based on the users clicked position, including the directions of sunrise and sunset. This allows for an approximate estimation of whether sunlight will be blocked by clouds at sunrise or sunset',
     author: 'Francesco Gola',
